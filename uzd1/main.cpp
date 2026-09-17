1222563396eb1bca861e830f02557d02eed79b2d
Lukas Mikalauskas <324364863+lumi512@users.noreply.github.com>
2026-09-17 09:27:18 +0300

    uzduotis 1

---
+++ uzd1/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string vardas = "Jonas";
+	string pavarde = "Joniukas";
+
+	int amzius = 19;
+	int grupe = 1;
+
+	string kursas = "Strukturinis programavimas";
+	string programa = "Programos pavadinimas";
+
+	cout << "Vardas: " << vardas << endl;
+	cout << "Pavarde: " << pavarde << endl;
+
+	cout << "Amzius: " << amzius << endl;
+	cout << "Grupe: " << grupe << endl;
+
+	cout << "Kursas: " << kursas << endl;
+	cout << "Programa: " << programa << endl;
+
+	return 0;
+}
